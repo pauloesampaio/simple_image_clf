563d95c89b9290a6d4e3b67d2fa104b360f7b2e5
pauloesampaio <paulo.ed@gmail.com>
2020-08-17 00:01:59 -0300

    Initial commit

---
+++ web/src/simple_image_clf.py
@@ -0,0 +1,45 @@
+import streamlit as st
+import pandas as pd
+from skimage.io import imread
+from tensorflow.image import resize
+from tensorflow.keras.applications.resnet import (ResNet50,
+                                                  decode_predictions,
+                                                  preprocess_input)
+
+
+def download_image(url, target_size=(224, 224)):
+    image = imread(url)
+    st.image(image, width=224)
+    resized_image = resize(image,
+                           target_size,
+                           antialias=True)
+    return resized_image.numpy()
+
+
+def preprocess_image(image):
+    reshaped_image = image.reshape((1,) + current_image.shape)
+    preprocessed_input = preprocess_input(reshaped_image)
+    return preprocessed_input
+
+
+def decode_result(prediction):
+    decoded_prediction = decode_predictions(prediction)
+    result_dict = pd.DataFrame(data=[w[2] for w in decoded_prediction[0]],
+                               index=[w[1] for w in decoded_prediction[0]],
+                               columns=["probability"])
+    st.write(result_dict)
+    return result_dict
+
+
+model = ResNet50()
+
+st.write('''
+# Simple image clf
+## Enter the image url
+''')
+url = st.text_input("Enter image url")
+if url:
+    current_image = download_image(url)
+    model_input = preprocess_image(current_image)
+    prediction = model.predict(model_input)
+    result = decode_result(prediction)
