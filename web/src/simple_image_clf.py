35b5e30d4c2673d7e66e91e26bfd812a88d7ae6b
pauloesampaio <paulo.ed@gmail.com>
2020-09-27 19:02:32 -0300

    changing to the more generic decode_image

--- web/src/simple_image_clf.py
+++ web/src/simple_image_clf.py
@@ -1,7 +1,7 @@
 import streamlit as st
 import pandas as pd
 import requests
-from tensorflow.image import resize, decode_jpeg
+from tensorflow.image import resize, decode_image
 from tensorflow.keras.applications.resnet import (
     ResNet50,
     decode_predictions,
